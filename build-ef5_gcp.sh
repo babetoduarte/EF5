b78ae9b729f626769f4676dfff51e149935f5c1b
Jorge A. Duarte G <jduarte@ou.edu>
2018-06-14 12:04:22 -0500

    Modified build file for normal compilation

--- build-ef5_gcp.sh
+++ build-ef5_gcp.sh
@@ -6,15 +6,17 @@
 #	../build-ef5_gcp.sh
 
 
-module load pgi/18.4 #openmpi/2.1.2/pgi/18.4
-spack load libgeotiff
+#module load pgi/18.4 #openmpi/2.1.2/pgi/18.4
+#spack load libgeotiff
 
-GEOTIFF=$(spack find --paths libgeotiff | tail -n 1 | awk '{print $2}')
+#GEOTIFF=$(spack find --paths libgeotiff | tail -n 1 | awk '{print $2}')
 
 cd .. && autoreconf --force --Install
 cd build
 #Compile for GPU
-CC=pgcc CXX=pgc++ LDFLAGS="-L${GEOTIFF}/lib -lgeotiff" CPPFLAGS="-I${GEOTIFF}/include" CXXFLAGS+=" -O3 -std=c++11 -Minfo=accel -Mlarge_arrays -ta=tesla:managed" ../configure
+#CC=pgcc CXX=pgc++ LDFLAGS="-L${GEOTIFF}/lib -lgeotiff" CPPFLAGS="-I${GEOTIFF}/include" CXXFLAGS+=" -O3 -std=c++11 -Minfo=accel -Mlarge_arrays -ta=tesla:managed" ../configure
 #Compile for CPU Multicore
 #CC=pgcc CXX=pgc++ LDFLAGS="-L${GEOTIFF}/lib -lgeotiff" CPPFLAGS="-I${GEOTIFF}/include" CXXFLAGS+=" -std=c++11 -Minfo=accel -ta=multicore" ../configure
+#Compile for CPU with OpenMP
+../configure
 make -j
